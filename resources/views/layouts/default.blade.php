63c43968b8aa697bfcfd08084d4ab55118819d7c
Fahadahmed23 <fahadahmedoptimist@gmail.com>
2021-11-04 20:16:48 +0500

    Facebook messenger

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -52,4 +52,34 @@
       src="https://www.facebook.com/tr?id=408775447605793&ev=PageView&noscript=1"
     /></noscript>
 <!-- End Facebook Pixel Code -->
+
+
+
+<!-- Messenger Chat Plugin Code -->
+<div id="fb-root"></div>
+
+<!-- Your Chat Plugin code -->
+<div id="fb-customer-chat" class="fb-customerchat">
+</div>
+
+<script>
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "1893736550865043");
+  chatbox.setAttribute("attribution", "biz_inbox");
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v12.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
 </html>
